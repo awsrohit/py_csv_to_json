1a53bed428e06ae0794cc1d192a3f649e24b486d
Sanjanagaddam <30503664+Sanjanagaddam@users.noreply.github.com>
2017-08-15 15:51:54 -0500

    Update csv_to_json.py

--- csv_to_json/csv_to_json.py
+++ csv_to_json/csv_to_json.py
@@ -1,13 +1,14 @@
 import csv
 import json
 
-with open('test.csv') as f:
-    reader = csv.DictReader(f)
-    rows = list(reader)
-    
+def main():
+    with open('test.csv') as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+    with open('test.json', 'w') as f:
+        json.dump(rows, f)
+        
 
-with open('test.json', 'w') as f:
-    json.dump(rows, f)
     
     
     
